70f149c75d8c3f8126212fedf252eb1c0bb17d47
ftomasello <32821413+franktomasello@users.noreply.github.com>
2020-01-02 14:33:39 -0800

    Initial commit

---
+++ HiC.c
@@ -0,0 +1,19 @@
+//
+// Created by frank on 12/30/2019.
+//
+
+#include <stdbool.h>
+#include <stdio.h>
+
+void three_Lines(void)
+{
+    printf("January\n");
+    printf("February\n");
+    printf("March\n");
+}
+
+void main(void)
+{
+    three_Lines();
+}
+
